5982bf0387f6df555487ba39733245ff11b36147
Nataly <ncgarzon88@gmail.com>
2018-08-12 22:01:20 -0400

    base html done and some a little sass

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,13 @@
-"use strict";console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+"use strict";
+
+console.log("Hell0");
+
+//idea for spinning performers
+// pull all the li(s) as an array.
+
+// if they are in the x spot(center spot depending on how many performers there are) of the array then give them a class of show-large if they aren't give them the class of show-small. You'll hide small and large accordingly and have them positioned ontop of each other
+
+//on click of any of them also does this..ie makes them go into the center spot and get the class of large
+
+//if there are more than 3 performers than hide the others (left/right) and push/pop the array items as it auto spins
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
-console.log(`Hello World from app.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+console.log(`Hell0`)
+
+//idea for spinning performers
+// pull all the li(s) as an array.
+
+// if they are in the x spot(center spot depending on how many performers there are) of the array then give them a class of show-large if they aren't give them the class of show-small. You'll hide small and large accordingly and have them positioned ontop of each other
+
+//on click of any of them also does this..ie makes them go into the center spot and get the class of large
+
+//if there are more than 3 performers than hide the others (left/right) and push/pop the array items as it auto spins
