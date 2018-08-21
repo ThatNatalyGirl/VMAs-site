a7b39529332ad668f13ffcdeba18a69e52a4b4c7
Nataly <ncgarzon88@gmail.com>
2018-08-20 22:55:08 -0400

    hover over works

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,4 +10,26 @@ console.log("Hell0");
 //on click of any of them also does this..ie makes them go into the center spot and get the class of large
 
 //if there are more than 3 performers than hide the others (left/right) and push/pop the array items as it auto spins
+
+// let hoverInfoImage = document.querySelectorAll("figure")
+var hoverInfoBox = document.querySelectorAll("figcaption");
+
+hoverInfoBox.forEach(function (box) {
+	var figCaption = box.parentNode;
+
+	figCaption.addEventListener("mouseover", function () {
+		console.log(box.parentNode);
+		console.log(box);
+
+		box.style.display = 'block';
+		box.style.opacity = '1';
+	});
+
+	figCaption.addEventListener("mouseleave", function () {
+		box.style.opacity = '0';
+		setTimeout(function () {
+			box.style.display = 'none';
+		}, 500);
+	});
+});
 //# sourceMappingURL=main.js.map
