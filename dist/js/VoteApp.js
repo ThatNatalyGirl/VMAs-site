53de3c08d9587bde2a1339c018b82fb42b4a9617
DannyMcMillan <mcmillandanny@gmail.com>
2018-09-07 22:09:08 -0400

    fixed some stuff

--- dist/js/VoteApp.js
+++ dist/js/VoteApp.js
@@ -3,28 +3,32 @@
 console.log("THIS IS THE Vote App");
 
 var url = 'http://localhost:1337/vote';
-var voteBtn = document.querySelector('.vote');
-console.log(voteBtn);
+var voteBtns = document.querySelectorAll('.vote');
+var API_BASE = 'http://159.89.151.127:1337/vote';
+console.log(voteBtns);
 
-var artistA = 0;
-
-voteBtn.addEventListener('click', function (plus) {
-  axios.post(url, {
-    artistA: artistA
-  }).then(function (response) {
-    var artistA = plus;
-    plus++;
-    console.log(response);
-  }).catch(function (error) {
-    console.log(error);
+for (var i = 0; i < voteBtns.length; i++) {
+  voteBtns[i].addEventListener('click', function () {
+    axios.post(url + "/0").then(function (response) {
+      console.log(response);
+    }).catch(function (error) {
+      console.log(error);
+    });
   });
-});
+}
 
-var getData = function getData() {
-  axios.get('http://localhost:1337').then(function (response) {
-    console.log(response);
-  }).catch(function (error) {
-    console.log(error);
-  });
-};
+// let getData = function() {
+// 	axios.get(url + '/0')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+// }
+
+// voteBtns.forEach(function(voteBtn, i) {
+//   console.log( voteBtn, i )
+
+// })
 //# sourceMappingURL=VoteApp.js.map
--- src/js/VoteApp.js
+++ src/js/VoteApp.js
@@ -1,34 +1,41 @@
 console.log("THIS IS THE Vote App")
 
 let url = 'http://localhost:1337/vote';
-let voteBtn = document.querySelector('.vote');
-console.log(voteBtn);
+let voteBtns = document.querySelectorAll('.vote');
+const API_BASE = 'http://159.89.151.127:1337/vote';
+console.log(voteBtns);
 
-let artistA = 0;
-
-voteBtn.addEventListener('click', function(plus) {
-	axios
-		.post(url, {
-			artistA: artistA
-	})
-		.then(function (response) {
-			let artistA = plus;
-				plus++
-       		console.log(response)
+for (var i = 0; i < voteBtns.length; i++) {
+  voteBtns[i].addEventListener('click', function() {
+    axios
+      .post(url + "/0")
+      .then(function (response) {
+        console.log(response)
     })
-    	.catch(function (error) {
-        	console.log(error)
+      .catch(function (error) {
+        console.log(error)
+
     });
-});
+  
+  });
 
-
-let getData = function() {
-	axios.get('http://localhost:1337')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
 }
+
+
+
+
+// let getData = function() {
+// 	axios.get(url + '/0')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+// }
+
+// voteBtns.forEach(function(voteBtn, i) {
+//   console.log( voteBtn, i )
+
+// })
 
