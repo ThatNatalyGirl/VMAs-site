466031163b7b8c3fcd3375712a9dc30d9ff7c4a0
DannyMcMillan <mcmillandanny@gmail.com>
2018-09-13 03:15:40 -0400

    fixing commentApp

--- dist/js/VoteApp.js
+++ dist/js/VoteApp.js
@@ -2,14 +2,18 @@
 
 console.log("THIS IS THE Vote App");
 
-var LOCAL_URL = 'http://localhost:1337/vote'; //for development
-var API_BASE = 'http://159.89.151.127:1337/vote'; // for production
+var LOCAL_URL = 'http://localhost:1337/vote';
 var voteBtns = document.querySelectorAll('.vote');
+var API_BASE = 'http://159.89.151.127:1337/vote';
+console.log(voteBtns);
 
 voteBtns.forEach(function (voteBtn, i) {
   voteBtn.addEventListener('click', function () {
 
-    axios.post(API_BASE + "/" + i).then(function (response) {
+    if (this.getAttribute("disabled")) return;
+    axios.post(LOCAL_URL + "/" + i).then(function (response) {
+      console.log(response.data);
+      // display the votes on the page
       var voteTotals = response.data;
       voteBtns.forEach(function (voteBtn, j) {
         voteBtn.setAttribute("disabled", "disabled");
--- src/js/VoteApp.js
+++ src/js/VoteApp.js
@@ -1,29 +1,30 @@
 console.log("THIS IS THE Vote App")
 
-const LOCAL_URL = 'http://localhost:1337/vote'; //for development
-const API_BASE = 'http://159.89.151.127:1337/vote'; // for production
+let LOCAL_URL = 'http://localhost:1337/vote';
 let voteBtns = document.querySelectorAll('.vote');
+const API_BASE = 'http://159.89.151.127:1337/vote';
+console.log(voteBtns);
 
 voteBtns.forEach(function(voteBtn, i) {
   voteBtn.addEventListener('click', function() {
 
+  if (this.getAttribute("disabled")) return
     axios
-      .post(API_BASE + "/" + i)
+      .post(LOCAL_URL + "/" + i)
       .then(function (response) {
+        console.log(response.data)
+        // display the votes on the page
         let voteTotals = response.data;
       voteBtns.forEach(function(voteBtn, j) {
-          voteBtn.setAttribute("disabled", "disabled")
-          voteBtn.innerHTML = voteTotals[j]
-        })
-
+        voteBtn.setAttribute("disabled", "disabled")
+        voteBtn.innerHTML = voteTotals[j]
       })
-      .catch(function (error) {
+    })
+           .catch(function (error) {
         console.log(error)
-
-    });
-  
-  });
+    })
+  })
+   
 })
-
 
 
